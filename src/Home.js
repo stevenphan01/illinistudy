58b744304d664c762cfc7703e83a1aa3ef39d308
mshah12 <megh5000@gmail.com>
2021-11-03 19:58:13 -0500

    finished :)

--- src/Home.js
+++ src/Home.js
@@ -7,38 +7,196 @@ import Axios from 'axios';
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    this.handleSearchUser = this.handleSearchUser.bind(this);
-    this.handleUserSubmit = this.handleUserSubmit.bind(this);
+    this.handleUserID = this.handleUserID.bind(this);
+    this.handleUsername = this.handleUsername.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+    this.handleEmail = this.handleEmail.bind(this);
+    this.handleAddress = this.handleAddress.bind(this);
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+    this.handleInsertSubmit = this.handleInsertSubmit.bind(this);
+    this.handleUpdateSubmit = this.handleUpdateSubmit.bind(this);
+    this.handleDeleteSubmit = this.handleDeleteSubmit.bind(this);
+    this.handleAdvancedQuery1 = this.handleAdvancedQuery1.bind(this);
+    this.handleAdvancedQuery2 = this.handleAdvancedQuery2.bind(this);
     this.state = {
-      "user": "",
-      "displayedUsers": []
+      "user_id": "",
+      "username": "",
+      "password": "",
+      "email": "",
+      "address": "",
     }
   }
-  handleUserSubmit(e) {
+
+  handleSearchSubmit(e) {
     e.preventDefault();
-    //this.setState({user:""});
-    /* Call api to search for user */
-    console.log(this.state.user)
     Axios.post('http://localhost:3002/api/getuser', {
-      username: this.state.user,
+      username: this.state.username,
     }).then((e) => {
       e.data.map((result) =>{
         return console.log(result);
-      }
-      )
-      //alert('success insert')
+      })
     })
+    this.setState({
+      user_id: "",
+      username: "",
+      password: "",
+      email: "",
+      address: "",
+    });
   }
-  handleSearchUser(e) {
-    this.setState({user: e.target.value});
+
+  handleInsertSubmit(e) {
+    e.preventDefault();
+    Axios.post('http://localhost:3002/api/insert', {
+      user_id: this.state.user_id,
+      username: this.state.username,
+      password: this.state.password,
+      email: this.state.email,
+      address: this.state.address,
+    }).then((e) => {
+      console.log(e);
+    })
+    this.setState({
+      user_id: "",
+      username: "",
+      password: "",
+      email: "",
+      address: "",
+    });
   }
+
+  handleUpdateSubmit(e) {
+    e.preventDefault();
+    Axios.post('http://localhost:3002/api/update', {
+      user_id: this.state.user_id,
+      password: this.state.password,
+    }).then((e) => {
+      console.log(e);
+    })
+    this.setState({
+      user_id: "",
+      username: "",
+      password: "",
+      email: "",
+      address: "",
+    });
+  }
+
+  handleDeleteSubmit(e) {
+    e.preventDefault();
+    Axios.post('http://localhost:3002/api/delete', {
+      user_id: this.state.user_id,
+      password: this.state.password,
+    }).then((e) => {
+      console.log(e);
+    })
+      this.setState({
+      user_id: "",
+      username: "",
+      password: "",
+      email: "",
+      address: "",
+    });
+  }
+
+  handleAdvancedQuery1(e) {
+    e.preventDefault();
+    Axios.get('http://localhost:3002/api/adv1', {
+    }).then((e) => {
+      console.log(e);
+    })
+      this.setState({
+      user_id: "",
+      username: "",
+      password: "",
+      email: "",
+      address: "",
+    });
+  }
+
+  handleAdvancedQuery2(e) {
+    e.preventDefault();
+    Axios.get('http://localhost:3002/api/adv2', {
+    }).then((e) => {
+      console.log(e);
+    })
+      this.setState({
+      user_id: "",
+      username: "",
+      password: "",
+      email: "",
+      address: "",
+    });
+  }
+
+  handleUserID(e) {
+    this.setState({user_id: e.target.value});
+  }
+  handleUsername(e) {
+    this.setState({username: e.target.value});
+  }
+  handlePassword(e) {
+    this.setState({password: e.target.value});
+  }
+  handleEmail(e) {
+    this.setState({email: e.target.value});
+  }
+  handleAddress(e) {
+    this.setState({address: e.target.value});
+  }
+
   render() {
-    //Change the userSearch to locSearch eventually
     return (<React.Fragment>
+              <h1>Search</h1>
               <div className="userSearch" >
-                  <button value="Submit" className="submitUser" onClick={(e)=>this.handleUserSubmit(e)}/>
-                    <label>Search User: </label>
-                    <input type="text" value={this.state.user} onChange={(e)=>this.handleSearchUser(e)}/>
+                    <label>Search by username: </label>
+                    <input type="text" value={this.state.username} onChange={(e)=>this.handleUsername(e)}/>
+                    <br/>
+                    <br/>
+                    <button className="submitUser" onClick={(e)=>this.handleSearchSubmit(e)}>Submit</button>
+              </div>
+              <h1>Insert</h1>
+              <div className="userInsert" >
+                    <label>UserID: </label>
+                    <input type="text" value={this.state.user_id} onChange={(e)=>this.handleUserID(e)}/>
+                    <label> Username: </label>
+                    <input type="text" value={this.state.username} onChange={(e)=>this.handleUsername(e)}/>
+                    <label> Password: </label>
+                    <input type="text" value={this.state.password} onChange={(e)=>this.handlePassword(e)}/>
+                    <label> Email: </label>
+                    <input type="text" value={this.state.email} onChange={(e)=>this.handleEmail(e)}/>
+                    <label> Address: </label>
+                    <input type="text" value={this.state.address} onChange={(e)=>this.handleAddress(e)}/>
+                    <br/>
+                    <br/>
+                    <button className="submitUser" onClick={(e)=>this.handleInsertSubmit(e)}>Submit</button>
+              </div>
+              <h1>Update</h1>
+              <div className="userUpdate" >
+                    <label>UserID: </label>
+                    <input type="text" value={this.state.user_id} onChange={(e)=>this.handleUserID(e)}/>
+                    <label> New Password: </label>
+                    <input type="text" value={this.state.password} onChange={(e)=>this.handlePassword(e)}/>
+                    <br/>
+                    <br/>
+                    <button className="submitUser" onClick={(e)=>this.handleUpdateSubmit(e)}>Submit</button>
+              </div>
+              <h1>Delete</h1>
+              <div className="userDelete" >
+                    <label>UserID: </label>
+                    <input type="text" value={this.state.user_id} onChange={(e)=>this.handleUserID(e)}/>
+                    <label> Password: </label>
+                    <input type="text" value={this.state.password} onChange={(e)=>this.handlePassword(e)}/>
+                    <br/>
+                    <br/>
+                    <button className="submitUser" onClick={(e)=>this.handleDeleteSubmit(e)}>Submit</button>
+              </div>
+              <h1>Advanced Queries</h1>
+              <div className="userAdvancedQueries" >
+                    <button className="submitUser" onClick={(e)=>this.handleAdvancedQuery1(e)}>Advanced Query 1</button>
+                    <br/>
+                    <br/>
+                    <button className="submitUser" onClick={(e)=>this.handleAdvancedQuery2(e)}>Advanced Query 2</button>
               </div>
               <MapView/>
               <ListView/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -12,18 +12,9 @@ var db = mysql.createConnection({
     database:'pt1',
 })
 
-// return db;
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-
-// app.get('/', (require, response) => {
-//     const sqlRequest = "SELECT * FROM pt1.users LIMIT 0,10;";
-//     db.query(sqlRequest, (err, result) => {
-//         response.send(result);
-//     })
-// })
 
 app.post('/api/getuser', (require, response) => {
     const username = require.body.username;
@@ -42,40 +33,57 @@ app.post("/api/insert", (require, response) => {
 
     const sqlInsert = "INSERT INTO pt1.users (`user_id`, `password`, `username`, `email`, `address`) VALUES (?,?,?,?,?);";
     db.query(sqlInsert, [user_id, password, username, email, address], (err, result) => {
-        //console.log(err);
-        //response.send("fail");
+        response.send("Inserted!");
     })
 });
 
 app.post("/api/update", (require, response) => {
     const user_id = require.body.user_id;
-    const password = "blah";
-    // const username = "username";
-    // const email = "email@email.com";
-    // const address = "123 North Champaign Road, Champaign IL, 61820";
+    const password = require.body.password;
 
     const sqlUpdate = "UPDATE pt1.users SET pt1.users.password = ? WHERE pt1.users.user_id = ?;";
     db.query(sqlUpdate, [password, user_id], (err, result) => {
-        //console.log(err);
-        //response.send("fail");
-        response.send("update")
+        response.send("Updated!");
     })
 });
 
 app.post("/api/delete", (require, response) => {
     const user_id = require.body.user_id;
-    // const password = "blah";
-    // const username = "username";
-    // const email = "email@email.com";
-    // const address = "123 North Champaign Road, Champaign IL, 61820";
-
-    const sqlUpdate = "DELETE FROM pt1.users WHERE pt1.users.user_id = ?;";
-    db.query(sqlUpdate, [user_id], (err, result) => {
-        //console.log(err);
-        //response.send("fail");
-        response.send("update")
+    const password = require.body.password;
+    const sqlUpdate = "DELETE FROM pt1.users WHERE pt1.users.user_id = ? AND pt1.users.password = ?;";
+    db.query(sqlUpdate, [user_id, password], (err, result) => {
+        response.send("Deleted!");
     })
 });
+
+app.get('/api/adv1', (require, response) => {
+    const sqlAdv1 = `(SELECT s.name, AVG(r.food_availibility) as food_avail
+                        FROM pt1.reviews as r NATURAL JOIN pt1.study_spaces s
+                        GROUP BY ss_id
+                        ORDER BY food_avail)
+                        UNION 
+                        (SELECT s.name, AVG(r.quietness) as quiet_level
+                        FROM pt1.reviews as r NATURAL JOIN pt1.study_spaces s 
+                        GROUP BY ss_id
+                        ORDER BY quiet_level
+                        );`;
+    db.query(sqlAdv1,(err, result) => {
+        response.send(result);
+    })
+})
+
+app.get('/api/adv2', (require, response) => {
+    const sqlAdv2 = `SELECT AVG(sg1.curr_capacity) as curr_cap
+                    FROM pt1.study_groups sg1 NATURAL JOIN (SELECT ss1.ss_id
+                                                            FROM pt1.study_spaces ss1
+                                                            WHERE ss1.open >= 7) as t1
+                    GROUP BY sg1.course
+                    HAVING curr_cap > 1
+                    ORDER BY curr_cap`;
+    db.query(sqlAdv2,(err, result) => {
+        response.send(result);
+    })
+})
 
 app.listen(3002, () => {
     console.log("running on port 3002");
